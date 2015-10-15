a873fedbd0188593aa8f89679f9b0b6f6d88f396
shy1 <sethyoungzip@gmail.com>
2015-10-15 16:11:24 -0400

    inputform functional

--- UnderwritingClientApp/app/scripts/controllers/about.js
+++ UnderwritingClientApp/app/scripts/controllers/about.js
@@ -8,10 +8,26 @@
  * Controller of the underwritingClientAppApp
  */
 angular.module('underwritingClientAppApp')
-  .controller('AboutCtrl', function () {
+  .controller('AboutCtrl', function ($scope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    $scope.clientData = {};
+    $scope.submitForm = function(isValid) {
+    	
+    	console.log("Hello");
+    	if (isValid) {
+    		$scope.clientData.name = $scope.name;
+    		$scope.clientData.age = $scope.age;
+    		$scope.clientData.phone = $scope.phone_number;
+    		$scope.clientData.income = $scope.income;
+    		$scope.clientData.debt = $scope.debt;
+    		$scope.clientData.home = $scope.home_value;
+    		$scope.clientData.car = $scope.car_value;
+    		$scope.clientData.savings = $scope.savings_investment_value;
+    		console.log($scope.clientData);	
+    	}
+    }
   });
