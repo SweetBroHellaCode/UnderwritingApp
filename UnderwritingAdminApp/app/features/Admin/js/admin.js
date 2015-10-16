8574f7f25223844312d4c0a924958ed47d49138f
SweetBroHellaCode <etmaron@gmail.com>
2015-10-16 12:08:59 -0400

    Fixed validation issues with Admin ID, now converts to JSON properly

--- UnderwritingAdminApp/app/features/Admin/js/admin.js
+++ UnderwritingAdminApp/app/features/Admin/js/admin.js
@@ -14,47 +14,61 @@ angular.module('underwritingAdminAppApp')
       'AngularJS',
       'Karma'
     ];
-
+$scope.user = {
+  incomeMin: 0,
+  incomeMax: 0,
+  debtMin: 0,
+  debtMax: 0,
+  creditScoreMin: 0,
+  creditScoreMax: 0,
+  ageMin: 0,
+  ageMax: 0,
+  adminId: "Enter ID Here!",
+};
 
 $scope.update = function(user) {
-try {
-  var validFields = true;
 
+  var validFields = true;
 
 // Check for valid ranges in fields then
 // submit information to server
-    if(document.getElementById("AdminId") === "Enter ID Here!") {
+    if($scope.user == undefined) {
+      alert("Please review fields to ensure entries");
+    }
+
+    if($scope.user.adminId == "Enter ID Here!") {
       validFields = false;
       alert("No Identification provided!");
     }
-    if($scope.incomeMin > $scope.incomeMax) {
+
+    if($scope.user.incomeMin > $scope.user.incomeMax) {
       validFields = false;
       alert("Your Annual Income Minimum is greater then your Annual Income Maximum!");
     }
 
-		if($scope.user.debtMin > $scope.user.debtMax) {
+		if($scope.debtMin > $scope.debtMax) {
       validFields = false;
 			alert("Your Debt Minimum is greater then your Debt Maximum!");
 		} 
 
-		if($scope.user.creditScoreMin > $scope.user.creditScoreMax) {
+		if($scope.creditScoreMin > $scope.creditScoreMax) {
       validFields = false;
 			alert("Your Credit Score Maximum is greater then your Annual Income Maximum!");
 		}
 
-    if ($scope.user.ageMin > $scope.user.ageMax) {
+    if ($scope.ageMin > $scope.ageMax) {
       validFields = false;
       alert("You Age Minimum is greater then your Age Maximum!")
     };        
 //If the above checks out, then send off data object
-    if (validFields) {
+    if (validFields && user != undefined) {
       console.log(user);
         //$http.post(url, data);   
-    };     
+    } else {
+      alert("Please review fields to ensure valid entry.")
+    }   
     
-  } catch(err) {
-    alert("Please review all fields for valid respones");
-  }
+  
 }
 
 });
