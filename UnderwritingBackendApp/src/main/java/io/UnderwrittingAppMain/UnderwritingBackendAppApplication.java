efcad17462a8f65bcae23c607a8e75b1db3ef9cb
SweetBroHellaCode <etmaron@gmail.com>
2015-10-16 14:38:22 -0400

    Set up folder structure

---
+++ UnderwritingBackendApp/src/main/java/io/UnderwrittingAppMain/UnderwritingBackendAppApplication.java
@@ -0,0 +1,12 @@
+package io.UnderwrittingAppMain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnderwritingBackendAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnderwritingBackendAppApplication.class, args);
+    }
+}
