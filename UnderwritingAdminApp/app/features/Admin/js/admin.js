ecb5778041a4c92eeefd37e7061f47b7dd0e9ed9
SweetBroHellaCode <etmaron@gmail.com>
2015-10-16 00:09:58 -0400

    Added fields and validation for user inputs, as well as fixed the submit function so it returns JSON data

--- UnderwritingAdminApp/app/features/Admin/js/admin.js
+++ UnderwritingAdminApp/app/features/Admin/js/admin.js
@@ -15,10 +15,47 @@ angular.module('underwritingAdminAppApp')
       'Karma'
     ];
 
+
 $scope.update = function(user) {
-        console.log("Hello");
-       $scope.master = angular.copy(user);
-      };
-  
-  });
+try {
+  var validFields = true;
+
+
+// Check for valid ranges in fields then
+// submit information to server
+    if(document.getElementById("AdminId") === "Enter ID Here!") {
+      validFields = false;
+      alert("No Identification provided!");
+    }
+    if($scope.incomeMin > $scope.incomeMax) {
+      validFields = false;
+      alert("Your Annual Income Minimum is greater then your Annual Income Maximum!");
+    }
+
+		if($scope.user.debtMin > $scope.user.debtMax) {
+      validFields = false;
+			alert("Your Debt Minimum is greater then your Debt Maximum!");
+		} 
+
+		if($scope.user.creditScoreMin > $scope.user.creditScoreMax) {
+      validFields = false;
+			alert("Your Credit Score Maximum is greater then your Annual Income Maximum!");
+		}
+
+    if ($scope.user.ageMin > $scope.user.ageMax) {
+      validFields = false;
+      alert("You Age Minimum is greater then your Age Maximum!")
+    };        
+//If the above checks out, then send off data object
+    if (validFields) {
+      console.log(user);
+        //$http.post(url, data);   
+    };     
+    
+  } catch(err) {
+    alert("Please review all fields for valid respones");
+  }
+}
+
+});
 
